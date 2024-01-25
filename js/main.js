b444b454b5528eb76e8f490f503ce6f08005f439
Patricia Atanasoff <system.informatica365@gmail.com>
2024-01-24 22:27:27 -0300

    overlay, logo, styles

---
+++ js/main.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const toggleButton = document.querySelector(".burger")
+    let isOpen = false
+
+
+    const timeline = gsap.timeline({ paused: true })
+
+    timeline.to(".block", {
+        duration: 1,
+        clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+        stagger: 0.075,
+        ease: "power3.inOut"
+    })
+
+    timeline.to(".menu-title, .menu-item", {
+        duration: .3,
+        opacity: 1,
+        stagger: .05
+    },
+        "-=.5"
+    )
+
+    toggleButton.addEventListener("click", function () {
+        if (isOpen) {
+            timeline.reverse()
+        } else {
+            timeline.play()
+        }
+        isOpen = !isOpen
+    })
+
+})
